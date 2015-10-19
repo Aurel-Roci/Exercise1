5350ce248051d113bd04de8bf960a391e6b9ce9f
Aurel-Roci <aurelroci@hotmail.com>
2015-10-19 21:42:19 +0200

    msg changed

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -9,15 +9,15 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello ASE2015 - how are you? :)");
+		System.out.println("Hello ASE2015 - how are you now? :)");
 
-		String user;
+		/*String user;
 		Scanner cin =  new Scanner(System.in);
 		System.out.println("Please enter you name: ");
 		HelloUser obj = new HelloUser(); 
 		user =  cin.next();
 		obj.setUser(user);
-		obj.greetUser();
+		obj.greetUser();*/
 	}
 	
 }
